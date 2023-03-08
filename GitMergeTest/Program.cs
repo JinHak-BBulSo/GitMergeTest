eb5e0863c7bab9775fe19d452f62e0dd74001b74
BBulSo <108716571+BBulSo@users.noreply.github.com>
2023-03-08 11:42:32 +0900

    [Alpha} midified class Ruler

--- GitMergeTest/Program.cs
+++ GitMergeTest/Program.cs
@@ -3,15 +3,7 @@ using System.Collections.Generic;
 
 namespace GitMergeTest
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Ruler ruler = new Ruler(10);
-            ruler.Run();
-        }
-    }
-
+    // Ruler 클래스 위치 수정
     public class Ruler
     {
         private const float ONE_INCH = 2.54F;
@@ -25,8 +17,16 @@ namespace GitMergeTest
 
         public void Run()
         {
-            Console.WriteLine($"{this.Centimeter}cm 는 {this.INCH}inch 입니다.");
+            Console.WriteLine($"{this.Centimeter}츠 sms {this.INCH}inch 입니다.");
         }
     }
-    
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ruler ruler = new Ruler(10);
+            ruler.Run();
+        }
+    }
 }
